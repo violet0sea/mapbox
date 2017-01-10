6e46632b6dfe60e1fb4c379e2943ac733f89168d
FunctionRun <545418969@qq.com>
2017-01-10 10:23:50 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+/*
+* @Author: FunctionRun
+* @Date:   2017-01-10 10:15:18
+* @Last Modified by:   FunctionRun
+* @Last Modified time: 2017-01-10 10:15:18
+* @Email: zhangyujie3344521@163.com
+* @File Path: /Users/zhangyujie/GitHub/FEscaffold/webpack.js
+* @File Name: webpack.js
+* @Descript:
+*/
+
+'use strict';
