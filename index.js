aec4356fd34aa7fd45fb9dd91a9cd7e814f2766b
FunctionRun <545418969@qq.com>
2017-01-10 11:44:59 +0800

    构建项目目录

---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+* @Author: FunctionRun
+* @Date:   2017-01-10 10:15:25
+* @Last Modified by:   FunctionRun
+* @Last Modified time: 2017-01-10 11:40:40
+* @Email: zhangyujie3344521@163.com
+* @File Path: /Users/zhangyujie/GitHub/FEscaffold/index.js
+* @File Name: index.js
+* @Descript:
+*/
+
+'use strict';
+
+console.log('项目测试成功');
+
+const author = 'zhangyujie';
+
+setTimeout(()=> {
+
+    console.log(author);
+
+}, 4000);
